66ecf64b67ab190c4fe8602d0cac67258f1f1604
Pratyushachitta <101507137+Pratyushachitta@users.noreply.github.com>
2022-04-24 23:48:16 +0530

    Update DoorSensor.h

--- 3_Implementation/inc/DoorSensor.h
+++ 3_Implementation/inc/DoorSensor.h
@@ -1,1 +1,11 @@
+#ifndef DOORSENSOR_H_INCLUDED
+#define DOORSENSOR_H_INCLUDED
 
+
+#include <avr/io.h>   // This header file includes the appropriate Input/output definitions for the device
+#include <stdio.h>    //stdio. h is a header file which has the necessary information to include the input/output related functions in our program.
+
+
+
+
+#endif // DOORSENSOR_H_INCLUDED
